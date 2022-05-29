ce2dc76f986e70ac9cf5c2548a12e530f2e839eb
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-05-30 01:57:48 +0530

    Leetcode Problem - Swap Nodes In Pairs

--- SwapNodesInPairs.java
+++ SwapNodesInPairs.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class SwapNodesInPairs {
+class SwapNodesInPairs {
+    public static void main(String[] args) {
+        ListNode list = new ListNode(0),p=list;
+        int n=5;
+        for (int i=1;i<=n;i++){
+            p.next = new ListNode(i);
+            p=p.next;
+        }
+        list = swapPairs(list.next);
+        while(list!=null){
+            System.out.print(list.val+" ");
+            list=list.next;
+        }
+    }
+    public static ListNode swapPairs(ListNode head) {
+        ListNode current=head,ans=new ListNode(0,head),next,prev;
+        prev=ans;
+        while(current!=null&&current.next!=null){
+            next = current.next;
+            prev.next = next;
+            current.next = next.next;
+            next.next = current;
+            prev=current;
+            current = current.next;
+        }
+        return ans.next;
+    }
 }
